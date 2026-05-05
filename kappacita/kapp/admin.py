04cad116677590eae217451ddc926d18a06ccd76
SeroSp <paulohenrique140307@gmail.com>
2026-05-04 22:15:24 -0400

    adcionado a Header nas telas, criei profissao do banco de dados, e comecei a modelar a tela de profissoes

--- kappacita/kapp/admin.py
+++ kappacita/kapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Profissao
 
-# Register your models here.
+admin.site.register(Profissao)
--- kappacita/kapp/models.py
+++ kappacita/kapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+class Profissao(models.Model):
+    nome = models.CharField(max_length=100)
+    area = models.CharField(max_length=100)
+    salario_min = models.DecimalField(max_digits=10, decimal_places=2)
+    salario_max = models.DecimalField(max_digits=10, decimal_places=2)
+    icone = models.ImageField(max_length=50)
+    estrelas = models.IntegerField(default=4) 
 
-# Create your models here.
+    def __str__(self):
+        return self.nome

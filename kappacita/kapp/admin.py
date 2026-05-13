91399a0ee864fafe3781b5d6ae38ecff5b2e8320
SeroSp <paulohenrique140307@gmail.com>
2026-05-12 20:53:31 -0400

    Alteraçoes no banco de dados para a criação de dois tipos diferente de categorias(para Cursos e para Profissoes)

--- kappacita/kapp/admin.py
+++ kappacita/kapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profissao, Curso, Categoria, Favorito
+from .models import Profissao, Curso, CategoriaCurso, CategoriaProfissao, Favorito
 
 admin.site.register(Profissao)
 admin.site.register(Curso)
-admin.site.register(Categoria)
+admin.site.register(CategoriaCurso)
+admin.site.register(CategoriaProfissao)
 admin.site.register(Favorito)

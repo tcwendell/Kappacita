2e33c9f57d61f4f0708dd7c97c6463b34a42248d
SeroSp <paulohenrique140307@gmail.com>
2026-06-01 15:32:08 -0400

    Mudanças em Login/Cadastro

--- kappacita/kappacita/settings.py
+++ kappacita/kappacita/settings.py
@@ -49,7 +49,7 @@ ROOT_URLCONF = 'kappacita.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'kapp' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
